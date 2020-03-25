c25a0c2f6129a89b1f6cd1314aacb8465bb99ec8
BjornGudmundsson <bjorn.gudmundsson95@gmail.com>
2020-03-25 20:56:26 +0100

    Added that you can take a password and turn it into a private key

---
+++ crypto/password.go
@@ -0,0 +1,21 @@
+package crypto
+
+import (
+	"crypto/sha256"
+	"encoding/binary"
+	"github.com/BjornGudmundsson/p2pBackup/files"
+	"github.com/BjornGudmundsson/p2pBackup/kyber"
+	"github.com/BjornGudmundsson/p2pBackup/purb/purbs"
+	"golang.org/x/crypto/pbkdf2"
+)
+
+//PrivateKeyFromPassword takes in a password and a given ciphersuite and returns
+//a scalar(a privatekey) derived from that password.
+func PrivateKeyFromPassword(pw string,suite purbs.Suite) (kyber.Scalar, error) {
+	salt := sha256.New().Sum([]byte(pw))
+	key := pbkdf2.Key([]byte(pw), salt, files.ITERATIONS, files.KEYLEN, sha256.New)
+	v := binary.BigEndian.Uint64(key)
+	sk := suite.Scalar()
+	sk.SetInt64(int64(v))
+	return sk, nil
+}
---
+++ crypto/password_test.go
@@ -0,0 +1,58 @@
+package crypto
+
+import (
+	"fmt"
+	"github.com/BjornGudmundsson/p2pBackup/kyber/util/random"
+	"github.com/BjornGudmundsson/p2pBackup/purb"
+	"github.com/BjornGudmundsson/p2pBackup/purb/purbs"
+	"github.com/stretchr/testify/assert"
+	"go.dedis.ch/kyber/v3/group/curve25519"
+	"testing"
+)
+
+func TestPrivateKeyFromPassword(t *testing.T) {
+	pw := "Bjorn"
+	suite, e := purb.GetSuite(curve25519.NewBlakeSHA256Curve25519(true).String())
+	assert.Nil(t, e, "Should be able to get Suite")
+	sk, e := PrivateKeyFromPassword(pw, suite)
+	assert.Nil(t, e, "Should be able to generate a scalar")
+	assert.NotNil(t, sk, "Private key should be non-nil")
+	p := suite.Point().Base()
+	pk := p.Mul(sk, p)
+	assert.NotNil(t, pk, "Public key should be non-nil")
+}
+
+func TestPasswordToPURB(t *testing.T) {
+	toEncrypt := "I sell seashells by the seashore"
+	pw := "Bjorn"
+	suite, e := purb.GetSuite(curve25519.NewBlakeSHA256Curve25519(true).String())
+	assert.Nil(t, e, "Should be able to generate a suite")
+	assert.NotNil(t, suite, "Suite should be non-nil")
+	sk, e := PrivateKeyFromPassword(pw, suite)
+	assert.Nil(t, e, "Should be able to get private key from scalar")
+	assert.NotNil(t, sk, "Private key should be non-nil")
+	fmt.Println(sk.String())
+	p := suite.Point().Base()
+	fmt.Println("mul")
+	pk := p.Mul(sk, p)
+	fmt.Println("Mul over")
+	m, e := pk.MarshalBinary()
+	assert.Nil(t, e, "Should be able to marshal the public key")
+	recipient, e := purb.NewRecipient(m, suite)
+	assert.Nil(t, e, "should be able to generate a recipient")
+	suiteInfoMap := purb.GetSuiteInfos("")
+	params := purbs.NewPublicFixedParameters(suiteInfoMap, false)
+	recipients := []purbs.Recipient{recipient}
+	data, e := purbs.Encode([]byte(toEncrypt), recipients, random.New(), params, false)
+	assert.Nil(t, e, "should be able to encode the PURB")
+	msk, e := sk.MarshalBinary()
+	assert.Nil(t, e, "Should be able to unmarshall binary key")
+	privRecipient, e := purb.NewPrivateRecipient(msk, suite)
+	assert.Nil(t, e, "should be able to get a private recipient")
+	blob := data.ToBytes()
+	success, decrypted, e := purbs.Decode(blob, &privRecipient,params, false)
+	assert.Nil(t, e , "Should be able to decode the PURB")
+	assert.True(t, success, "Should be able to decode the PURB")
+	assert.Equal(t, string(decrypted), toEncrypt, "The decrypted data should be the same as the unencrypted data")
+	fmt.Println(string(decrypted))
+}
